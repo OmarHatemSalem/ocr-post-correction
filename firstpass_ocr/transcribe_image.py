bebfbeea607ee2e0c06b1c9f663b061e71c23b08
Omar Hatem <omarhatem2002@aucegypt.edu>
2023-06-29 13:38:31 +0300

    library_project

--- firstpass_ocr/transcribe_image.py
+++ firstpass_ocr/transcribe_image.py
@@ -15,54 +15,100 @@ import glob
 import os.path
 import json
 from collections import defaultdict
-from google.cloud import vision
-from google.protobuf.json_format import MessageToJson
+# from google.cloud import vision
+#from google.protobuf.json_format import MessageToJson
+
+from PIL import Image
+import codecs
+import pyocr
+import pyocr.builders
 
 
 class OCR:
+    lang = 'ara'
     def __init__(self):
-        self.client = vision.ImageAnnotatorClient()
+        self.client = pyocr.get_available_tools()[0]
+        print("Will use tool '%s'" % (self.client.get_name()))
+        # Ex: Will use tool 'libtesseract'
+
+        langs = self.client.get_available_languages()
+        print("Available languages: %s" % ", ".join(langs))
+        lang = langs[0]
+        print("Will use lang '%s'" % (lang))
+        # self.client = vision.ImageAnnotatorClient()
 
     def detect_text(self, image_path):
-        with io.open(image_path, "rb") as image_file:
-            content = image_file.read()
-        image = vision.Image(content=content)
-        response = self.client.document_text_detection(image=image)
-        return response
+        txt = self.client.image_to_string(
+            Image.open(image_path),
+            lang=self.lang,
+            builder=pyocr.builders.TextBuilder()
+        )
+
+        return txt
+        # with io.open(image_path, "rb") as image_file:
+        #     content = image_file.read()
+        # image = vision.Image(content=content)
+        # response = self.client.document_text_detection(image=image)
+        # return response
 
     def return_full_text(self, img_path):
-        response = self.detect_text(img_path)
-        return response.full_text_annotation.text
+        txt = self.client.image_to_string(
+            Image.open(img_path),
+            lang=self.lang,
+            builder=pyocr.builders.TextBuilder()
+        )
+
+        return txt
+        # response = self.detect_text(img_path)
+        # return response.full_text_annotation.text
 
     def return_json(self, img_path):
-        response = self.detect_text(img_path)
+        #response = self.detect_text(img_path)
+
+        builder = pyocr.builders.LineBoxBuilder()
+
+        response = self.client.image_to_string(
+            Image.open(img_path),
+            lang=self.lang,
+            builder=builder
+        )
+
         blocks = {}
         count = 0
-        for page in response.full_text_annotation.pages:
-            for block in page.blocks:
-                cur_block = {}
-                cur_block["bounding_box"] = []
-                for vertex in block.bounding_box.vertices:
-                    cur_block["bounding_box"].append((vertex.x, vertex.y))
-                cur_block["lang"] = []
-                for lang in block.property.detected_languages:
-                    cur_block["lang"].append((lang.language_code))
-                cur_block["text"] = ""
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            cur_block["text"] += symbol.text
-                            break_type = symbol.property.detected_break.type
-                            if break_type == 1 or break_type == 2:
-                                cur_block["text"] += " "
-                            elif break_type == 3 or break_type == 5:
-                                cur_block["text"] += "\n"
-                            elif break_type == 4:
-                                cur_block["text"] += "-\n"
-                blocks[count] = cur_block
-                count += 1
+        for word in response:
+            cur_block = {}
+            cur_block["bounding_box"] = list(word.position)
+            cur_block["lang"] = ['ara']
+            cur_block["text"] = word.content
+            blocks[count] = cur_block
+            count += 1
 
-        blocks["fulltext"] = response.full_text_annotation.text
+
+        # for page in response.full_text_annotation.pages:
+        #     for block in page.blocks:
+        #         cur_block = {}
+        #         cur_block["bounding_box"] = []
+        #         for vertex in block.bounding_box.vertices:
+        #             cur_block["bounding_box"].append((vertex.x, vertex.y))
+        #         cur_block["lang"] = []
+        #         for lang in block.property.detected_languages:
+        #             cur_block["lang"].append((lang.language_code))
+        #         cur_block["text"] = ""
+        #         for paragraph in block.paragraphs:
+        #             for word in paragraph.words:
+        #                 for symbol in word.symbols:
+        #                     cur_block["text"] += symbol.text
+        #                     break_type = symbol.property.detected_break.type
+        #                     if break_type == 1 or break_type == 2:
+        #                         cur_block["text"] += " "
+        #                     elif break_type == 3 or break_type == 5:
+        #                         cur_block["text"] += "\n"
+        #                     elif break_type == 4:
+        #                         cur_block["text"] += "-\n"
+        #         blocks[count] = cur_block
+        #         count += 1
+
+        blocks["fulltext"] = self.return_full_text(img_path)
         return blocks
 
 
@@ -73,6 +119,7 @@ def get_ocr(image_paths, json_out):
         print(img)
         if json_out:
             responses.append(ocr_client.return_json(img))
+            # print("reached here\n")
         else:
             responses.append(ocr_client.return_full_text(img))
     return responses
@@ -82,14 +129,18 @@ def write_outputs(image_paths, ocr_responses, output_folder, json_out):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     for img, ocr in zip(image_paths, ocr_responses):
-        filename, _ = os.path.splitext(img.split("/")[-1])
+        # print("Img")
+        # print(img)
+        filename, _ = os.path.splitext(img.split("\\")[-1])
+        # print("Filename")
+        # print(filename)
         if json_out:
-            with open("".join([output_folder, "/", filename, ".json"]), "w") as out:
+            with open("".join([output_folder, "\\", filename, ".json"]), mode="w", encoding="utf-8") as out:
                 json.dump(
                     ocr, out, separators=(",", ": "), ensure_ascii=False, indent=4
                 )
         else:
-            with open("".join([output_folder, "/", filename, ".txt"]), "w") as out:
+            with open("".join([output_folder, "\\", filename, ".txt"]), mode="w", encoding="utf-8") as out:
                 out.write(ocr)
 
 
@@ -109,7 +160,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.image_folder:
-        image_paths = sorted(glob.glob(args.image_folder + "/*.png"))
+        image_paths = sorted(glob.glob(args.image_folder + "/*.jpg"))
     else:
         image_paths = [args.image]
 
--- train_single-source_win.sh
+++ train_single-source_win.sh
@@ -1,16 +1,16 @@
-# --------------------- REQUIRED: Modify for each dataset and\or experiment ---------------------
+# --------------------- REQUIRED: Modify for each dataset and\\or experiment ---------------------
 
 # Set pretraining, training and development set files
-pretrain_src="sample_dataset\postcorrection\pretraining\pretrain_src1.txt"
+pretrain_src="sample_dataset\\postcorrection\\pretraining\\pretrain_src1.txt"
 
-train_src="sample_dataset\postcorrection\training\train_src1.txt"
-train_tgt="sample_dataset\postcorrection\training\train_tgt.txt"
+train_src="sample_dataset\\postcorrection\\training\\train_src1.txt"
+train_tgt="sample_dataset\\postcorrection\\training\\train_tgt.txt"
 
-dev_src="sample_dataset\postcorrection\training\dev_src1.txt"
-dev_tgt="sample_dataset\postcorrection\training\dev_tgt.txt"
+dev_src="sample_dataset\\postcorrection\\training\\dev_src1.txt"
+dev_tgt="sample_dataset\\postcorrection\\training\\dev_tgt.txt"
 
 # Set experiment parameters
-expt_folder="my_expt_singlesource\"
+expt_folder="my_expt_singlesource\\"
 
 dynet_mem=3000 # Memory in MB available for training
 
@@ -24,17 +24,17 @@ trained_model_name="my_trained_model"
 
 # Create experiment directories
 mkdir $expt_folder
-mkdir $expt_folder\debug_outputs
-mkdir $expt_folder\models
-mkdir $expt_folder\outputs
-mkdir $expt_folder\pretrain_logs
-mkdir $expt_folder\pretrain_models
-mkdir $expt_folder\train_logs
-mkdir $expt_folder\vocab
+mkdir $expt_folder\\debug_outputs
+mkdir $expt_folder\\models
+mkdir $expt_folder\\outputs
+mkdir $expt_folder\\pretrain_logs
+mkdir $expt_folder\\pretrain_models
+mkdir $expt_folder\\train_logs
+mkdir $expt_folder\\vocab
 
 
 # Denoise outputs for pretraining
-python utils\denoise_outputs.py \
+python utils\\denoise_outputs.py \
 --train_src1 $train_src \
 --train_tgt $train_tgt \
 --input $pretrain_src \
@@ -44,30 +44,30 @@ pretrain_tgt=$pretrain_src'.denoised'
 
 
 # Create character vocabulary for the post-correction model
-python postcorrection\create_vocab.py \
+python postcorrection\\create_vocab.py \
 --src1_files $train_src $dev_src \
 --tgt_files $train_tgt $dev_tgt \
---output_folder $expt_folder\vocab
+--output_folder $expt_folder\\vocab
 
 
 # Pretrain the model (add --dynet-gpu for using GPU)
-# See postcorrection\opts.py for all the options
-python postcorrection\multisource_wrapper.py \
+# See postcorrection\\opts.py for all the options
+python postcorrection\\multisource_wrapper.py \
 --dynet-mem $dynet_mem \
 --dynet-autobatch 1 \
 --pretrain_src1 $pretrain_src \
 --pretrain_tgt $pretrain_tgt \
 $params \
 --single \
---vocab_folder $expt_folder\vocab \
+--vocab_folder $expt_folder\\vocab \
 --output_folder $expt_folder \
 --model_name $pretrained_model_name \
 --pretrain_only
 
 
 # Load the pretrained model and train the model using manually annotated training data (add --dynet-gpu for using GPU)
-# See postcorrection\opts.py for all the options
-python postcorrection\multisource_wrapper.py \
+# See postcorrection\\opts.py for all the options
+python postcorrection\\multisource_wrapper.py \
 --dynet-mem $dynet_mem \
 --dynet-autobatch 1 \
 --train_src1 $train_src \
@@ -76,8 +76,8 @@ python postcorrection\multisource_wrapper.py \
 --dev_tgt $dev_tgt \
 $params \
 --single \
---vocab_folder $expt_folder\vocab \
+--vocab_folder $expt_folder\\vocab \
 --output_folder $expt_folder \
---load_model $expt_folder"\pretrain_models\"$pretrained_model_name \
+--load_model $expt_folder"\\pretrain_models\\"$pretrained_model_name \
 --model_name $trained_model_name \
 --train_only
